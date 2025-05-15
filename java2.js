51fab84918b1f43a14f1485ed22991fd05e69bd7
Edvaldo Rodrigues <86741844+edvaldor@users.noreply.github.com>
2025-05-14 22:10:19 -0300

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,16 @@
+/*este e um exemplo de como nao fazer*/
+
+/*declarando variáveis*/
+let altura;
+let peso;
+let imc;
+/*criando uma função para calculo do IMC*/
+/*comentario de bloco*/
+//comentario de linha
+function calcImc(altura, peso){
+    
+
+
+    imc = peso / (altura * altura);//calcular o IMC
+    window.alert('Seu IMC é:'+imc);//mostrando o imc em Janela
+}
